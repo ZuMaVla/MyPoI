ed6abe8e51dff61b50040da2578a34bb33522d34
ZuMaVla <marinazubialevich@gmail.com>
2025-05-27 23:04:55 +0100

    Added -=Noticeboard=- feature (on 'About' page)

--- src/controllers/about-controller.js
+++ src/controllers/about-controller.js
@@ -1,7 +1,98 @@
+import { db } from "../models/db.js";
+
 export const aboutController = {
   index: {
-    handler: function (request, h) {
-      return h.view("about-view", { title: "About MyPoI" });
+    handler: async function (request, h) {
+      let recentPlace = {};
+      let popularPlace = {};
+      let discussedPlace = {};
+      const places = await db.placeStore.getAllPlaces();
+      if (places.length > 0) {
+        recentPlace = places[places.length - 1];
+        let index = 0;
+        let rating = 0;
+        for (let i = 0; i < places.length; i++) {
+          if (averageRating(places[i]) > rating) {
+            rating = averageRating(places[i]);
+            index = i;
+          }
+        }
+        popularPlace = places[index];
+        index = 0;
+        let reviews = 0;
+        for (let i = 0; i < places.length; i++) {
+          if (places[i].reviews.length > reviews) {
+            reviews = places[i].reviews.length;
+            index = i;
+          }
+        }
+        discussedPlace = places[index];
+        console.log(discussedPlace);
+      }
+
+      const topReviewer = getTopReviewer(places);
+      const activeReviewer = await db.userStore.getUserById(topReviewer.id);
+      const activeReviewerName = activeReviewer.firstName + " " + activeReviewer.lastName;
+      const viewData = {
+        recentPlace: averageRating(recentPlace),
+        popularPlace: averageRating(popularPlace),
+        discussedPlace: averageRating(discussedPlace),
+        topReviewer: activeReviewerName,
+        reviews: topReviewer.count,
+        title: "About MyPoI/Noticeboard",
+      };
+
+      return h.view("about-view", viewData);
     },
   },
 };
+
+function averageRating(place) {
+  const ratings = place.ratings || [];
+  let averageRating = 0;
+  let votes = ratings.length;
+  if (votes > 0) {
+    let sum = 0;
+    for (let j = 0; j < ratings.length; j++) {
+      if (ratings[j].rating === 0) {
+        votes -= 1;
+      }
+      sum += ratings[j].rating;
+    }
+    if (votes !== 0) {
+      averageRating = (sum / ratings.length).toFixed(1);
+    } else {
+      averageRating = 0;
+    }
+    place.averageRating = averageRating;
+  }
+  return place;
+}
+
+function getTopReviewer(places) {
+  // Create an empty "dictionary" to count reviews per userId
+  const reviewCounts = {};
+
+  for (const place of places) {
+    for (const review of place.reviews || []) {
+      const userId = review.userId.toString();
+      if (reviewCounts[userId]) {
+        reviewCounts[userId]++;
+      } else {
+        reviewCounts[userId] = 1;
+      }
+    }
+  }
+
+  let maxUserId = null;
+  let maxCount = 0;
+  // Find userId with the maximum review count
+  for (const [userId, count] of Object.entries(reviewCounts)) {
+    if (count > maxCount) {
+      maxCount = count;
+      maxUserId = userId;
+    }
+  }
+
+  return { id: maxUserId, count: maxCount };
+}
